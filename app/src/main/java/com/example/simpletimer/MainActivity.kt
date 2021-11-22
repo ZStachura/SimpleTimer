ba4d51009edc4814eea6a098201b2bb10cbd4275
ZStachura <zmstachura@gmail.com>
2021-11-22 21:42:04 +0100

    1 -> Layouts for both orientations

--- app/src/main/java/com/example/simpletimer/MainActivity.kt
+++ app/src/main/java/com/example/simpletimer/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.simpletimer
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        var orientation=resources.getConfiguration().orientation
+        when(orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> setContentView(R.layout.activity_main)
+            Configuration.ORIENTATION_LANDSCAPE -> setContentView(R.layout.activity_main_landscape)
+        }
+
+}
 }
